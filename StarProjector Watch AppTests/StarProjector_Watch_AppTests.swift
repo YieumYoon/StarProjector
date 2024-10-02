939662c8e0eb800d4d83666513b9c49a7a91dfad
YieumYoon <junsueddie@gmail.com>
2024-10-02 13:22:26 +0900

    Initial Commit, first version tested on Apple Watch SE(1st gen)

---
+++ StarProjector Watch AppTests/StarProjector_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StarProjector_Watch_AppTests.swift
+//  StarProjector Watch AppTests
+//
+//  Created by Junsu Lee on 9/26/24.
+//
+
+import Testing
+@testable import StarProjector_Watch_App
+
+struct StarProjector_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
